f4d3cda54e2d27752035b8fe704afe317b028ebf
Michał Małek <miszu@Michas-MacBook-Pro.local>
2024-08-11 09:38:15 +0200

    RoadAssistant core module

---
+++ RoadAssistant/src/main/java/io/malek/roadassistant/RoadAssistantApplication.java
@@ -0,0 +1,13 @@
+package io.malek.roadassistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoadAssistantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoadAssistantApplication.class, args);
+    }
+
+}
