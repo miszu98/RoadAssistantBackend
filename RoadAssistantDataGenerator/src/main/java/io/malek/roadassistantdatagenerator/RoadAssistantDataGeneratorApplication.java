1c00ddfa68650b56c355ad364cbb3f4d249c9d0b
Michał Małek <miszu@Michas-MacBook-Pro.local>
2024-08-11 10:36:06 +0200

    add eureka server and module for generate data about road incidents

---
+++ RoadAssistantDataGenerator/src/main/java/io/malek/roadassistantdatagenerator/RoadAssistantDataGeneratorApplication.java
@@ -0,0 +1,13 @@
+package io.malek.roadassistantdatagenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoadAssistantDataGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoadAssistantDataGeneratorApplication.class, args);
+    }
+
+}
