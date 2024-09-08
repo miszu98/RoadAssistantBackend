f6365e3399b6d308041dda17bc05f6f1399bb3e2
Michał Małek <miszu@Michas-MacBook-Pro.local>
2024-09-08 19:36:41 +0200

    tests for UserCreationController

--- RoadAssistantAuthorization/src/test/java/io/malek/roadassistantauthorization/user/UserCreationControllerTest.java
+++ RoadAssistantAuthorization/src/test/java/io/malek/roadassistantauthorization/user/UserCreationControllerTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 
-import static io.malek.roadassistantauthorization.user.data_generators.UserCreationRequestDataGeneratorUtil.getCorrectUser;
+import static io.malek.roadassistantauthorization.user.data_generators.UserCreationRequestDataGeneratorUtil.*;
 import static org.hamcrest.Matchers.equalTo;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -23,7 +23,7 @@ class UserCreationControllerTest extends IntegrationTestConfig {
 
     @BeforeEach
     void setUp() {
-
+        userRepository.deleteAll();
     }
 
     @AfterEach
@@ -45,6 +45,102 @@ class UserCreationControllerTest extends IntegrationTestConfig {
                         status().isOk(), jsonPath("$.processStatus", equalTo(ProcessStatus.COMPLETED.name())));
 
         assertEquals(1, userRepository.findAll().size());
+    }
+
+    @Test
+    void shouldReturnInformationAboutCreationUserProcessFailureWhenNotProvideEmail() throws Exception {
+        final UserCreationRequest userCreationRequest = getUserWithNullEmail();
+
+        final String userCreationRequestJson = objectMapper.writeValueAsString(userCreationRequest);
+
+        mockMvc.perform(post(USER_CREATION_API_URL)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(userCreationRequestJson))
+                .andDo(print())
+                .andExpectAll(
+                        status().isOk(), jsonPath("$.processStatus", equalTo(ProcessStatus.FAILURE.name())));
+
+        assertEquals(0, userRepository.findAll().size());
+    }
+
+    @Test
+    void shouldReturnInformationAboutCreationUserProcessFailureWhenNotProvideFirstname() throws Exception {
+        final UserCreationRequest userCreationRequest = getUserWithNullFirstName();
+
+        final String userCreationRequestJson = objectMapper.writeValueAsString(userCreationRequest);
+
+        mockMvc.perform(post(USER_CREATION_API_URL)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(userCreationRequestJson))
+                .andDo(print())
+                .andExpectAll(
+                        status().isOk(), jsonPath("$.processStatus", equalTo(ProcessStatus.FAILURE.name())));
+
+        assertEquals(0, userRepository.findAll().size());
+    }
+
+    @Test
+    void shouldReturnInformationAboutCreationUserProcessFailureWhenNotProvideLastname() throws Exception {
+        final UserCreationRequest userCreationRequest = getUserWithNullLastName();
+
+        final String userCreationRequestJson = objectMapper.writeValueAsString(userCreationRequest);
+
+        mockMvc.perform(post(USER_CREATION_API_URL)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(userCreationRequestJson))
+                .andDo(print())
+                .andExpectAll(
+                        status().isOk(), jsonPath("$.processStatus", equalTo(ProcessStatus.FAILURE.name())));
+
+        assertEquals(0, userRepository.findAll().size());
+    }
+
+    @Test
+    void shouldReturnInformationAboutCreationUserProcessFailureWhenNotProvidePassword() throws Exception {
+        final UserCreationRequest userCreationRequest = getUserWithNullPassword();
+
+        final String userCreationRequestJson = objectMapper.writeValueAsString(userCreationRequest);
+
+        mockMvc.perform(post(USER_CREATION_API_URL)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(userCreationRequestJson))
+                .andDo(print())
+                .andExpectAll(
+                        status().isOk(), jsonPath("$.processStatus", equalTo(ProcessStatus.FAILURE.name())));
+
+        assertEquals(0, userRepository.findAll().size());
+    }
+
+    @Test
+    void shouldReturnInformationAboutCreationUserProcessFailureWhenNotProvideRepeatPassword() throws Exception {
+        final UserCreationRequest userCreationRequest = getUserWithNullRepeatPassword();
+
+        final String userCreationRequestJson = objectMapper.writeValueAsString(userCreationRequest);
+
+        mockMvc.perform(post(USER_CREATION_API_URL)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(userCreationRequestJson))
+                .andDo(print())
+                .andExpectAll(
+                        status().isOk(), jsonPath("$.processStatus", equalTo(ProcessStatus.FAILURE.name())));
+
+        assertEquals(0, userRepository.findAll().size());
+    }
+
+    @Test
+    void shouldReturnInformationAboutCreationUserProcessFailureWhenPasswordAreNotEqual() throws Exception {
+        final UserCreationRequest userCreationRequest = getUserWithDifferentPasswords();
+
+        final String userCreationRequestJson = objectMapper.writeValueAsString(userCreationRequest);
+
+        mockMvc.perform(post(USER_CREATION_API_URL)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(userCreationRequestJson))
+                .andDo(print())
+                .andExpectAll(
+                        status().isOk(), jsonPath("$.processStatus", equalTo(ProcessStatus.FAILURE.name())));
+
+        assertEquals(0, userRepository.findAll().size());
     }
 
 }
