ce476af9d4f30f0d9c7aa3fddaad7bf32bdae822
Michał Małek <miszu@Michas-MacBook-Pro.local>
2024-09-24 16:50:18 +0200

    init gateway

---
+++ RoadAssistantGateway/src/main/java/io/malek/roadassistantgateway/RoadAssistantGatewayApplication.java
@@ -0,0 +1,13 @@
+package io.malek.roadassistantgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoadAssistantGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoadAssistantGatewayApplication.class, args);
+	}
+
+}
---
+++ RoadAssistantGateway/src/test/java/io/malek/roadassistantgateway/RoadAssistantGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package io.malek.roadassistantgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoadAssistantGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
